4813af13562bd391c9b247aed47f79fb30e8430b
Seo, Sinhyeok <sinhyeok.seo@purplelabs.co.kr>
2024-01-15 01:31:11 +0900

    feat: Add version subcommand with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod version_command;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Version(VersionArgs),
+}
+
+#[derive(Args)]
+struct VersionArgs {
+    #[arg(short, long, default_value = "minor")]
+    scope: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Version(args) => { version_command::run(args); }
+    }
 }
---
+++ src/version_command.rs
@@ -0,0 +1,32 @@
+use crate::VersionArgs;
+
+pub(crate) fn run(args: &VersionArgs) {
+    let scope = args.scope.as_ref().unwrap().clone();
+    // TODO: fn latest_tag() -> String
+    let latest_tag = "v0.1.0".to_string();
+    let version = version(scope, latest_tag);
+
+    // TODO: fn branch_name() -> String
+    let branch_name = "develop".to_string();
+    // TODO: fn short_commit_sha() -> Option<String>
+    let short_commit_sha = Some("ahs9df9d".to_string());
+    let metadata = metadata(branch_name, short_commit_sha);
+
+    println!("{}{}", version, metadata)
+}
+
+fn version(scope: String, latest_tag: String) -> String {
+    println!("scope: {}, latest_tag: {}", scope, latest_tag);
+    "v0.2.0".to_string()
+}
+
+fn metadata(branch_name: String, short_commit_sha: Option<String>) -> String {
+    if branch_name == "develop" {
+        match short_commit_sha {
+            Some(short_commit_sha_ok) => { format!("-dev.{}", short_commit_sha_ok) },
+            None => { panic!("Not found short_commit_sha on develop branch") }
+        }
+    } else {
+        "".to_string()
+    }
+}

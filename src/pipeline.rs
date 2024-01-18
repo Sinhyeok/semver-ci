20a43422b1d34e14fedb7274fa84656acbb52138
Seo, Sinhyeok <sinhyeok.seo@purplelabs.co.kr>
2024-01-19 02:33:48 +0900

    refactor: Move "pipeline_info()" function to pipeline module

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,4 +1,25 @@
+use std::env;
+
 pub(crate) trait Pipeline {
     fn branch_name(&self) -> String;
     fn short_commit_sha(&self) -> String;
+}
+
+const GITLAB_CI: &str = "GITLAB_CI";
+const GITHUB_ACTIONS: &str = "GITHUB_ACTIONS";
+
+pub(crate) fn pipeline_info() -> (String, String) {
+    if env::var(GITLAB_CI).map_or(false, |v| v == "true") {
+        let gitlab_ci = crate::gitlab_ci::GitlabCI {};
+        (gitlab_ci.branch_name(), gitlab_ci.short_commit_sha())
+    } else if env::var(GITHUB_ACTIONS).map_or(false, |v| v == "true") {
+        let github_action = crate::github_action::GithubAction {};
+        (
+            github_action.branch_name(),
+            github_action.short_commit_sha(),
+        )
+    } else {
+        let git_repo = crate::git_repo::GitRepo {};
+        (git_repo.branch_name(), git_repo.short_commit_sha())
+    }
 }
--- src/version_command.rs
+++ src/version_command.rs
@@ -1,10 +1,6 @@
-use crate::git_repo::GitRepo;
-use crate::github_action::GithubAction;
-use crate::gitlab_ci::GitlabCI;
-use crate::pipeline::Pipeline;
+use crate::pipeline;
 use crate::semantic_version::SemanticVersion;
 use crate::{git_service, VersionArgs};
-use std::env;
 
 const DEFAULT_SEMANTIC_VERSION_TAG: &str = "v0.0.0";
 
@@ -20,7 +16,7 @@ pub(crate) fn run(args: &VersionArgs) {
         .unwrap_or_else(|| DEFAULT_SEMANTIC_VERSION_TAG.to_string());
     let version = version(scope, latest_tag);
 
-    let (branch_name, short_commit_sha) = pipeline_info();
+    let (branch_name, short_commit_sha) = pipeline::pipeline_info();
     let metadata = metadata(branch_name, short_commit_sha);
 
     println!("{}{}", version, metadata)
@@ -51,21 +47,5 @@ fn metadata(branch_name: String, short_commit_sha: String) -> String {
         format!("-dev.{}", short_commit_sha)
     } else {
         "".to_string()
-    }
-}
-
-fn pipeline_info() -> (String, String) {
-    if env::var("GITLAB_CI").map_or(false, |v| v == "true") {
-        let gitlab_ci = GitlabCI {};
-        (gitlab_ci.branch_name(), gitlab_ci.short_commit_sha())
-    } else if env::var("GITHUB_ACTIONS").map_or(false, |v| v == "true") {
-        let github_action = GithubAction {};
-        (
-            github_action.branch_name(),
-            github_action.short_commit_sha(),
-        )
-    } else {
-        let git_repo = GitRepo {};
-        (git_repo.branch_name(), git_repo.short_commit_sha())
     }
 }

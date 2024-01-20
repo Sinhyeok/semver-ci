a6218f3e4d4b535c757b60fa8354ac922c5792db
Seo, Sinhyeok <sinhyeok.seo@purplelabs.co.kr>
2024-01-20 17:10:42 +0900

    refactor: Declare semantic version tag pattern as constant

--- src/git_service.rs
+++ src/git_service.rs
@@ -1,10 +1,12 @@
 use git2::Repository;
 use regex::Regex;
 
+const SEMANTIC_VERSION_TAG_PATTERN: &str = r"^v?([0-9]+\.[0-9]+\.[0-9]+)$";
+
 pub(crate) fn last_semantic_version_tag() -> Option<String> {
     let repo = Repository::open(".").unwrap_or_else(|e| panic!("Failed to open git repo: {}", e));
 
-    let semantic_version_regex = Regex::new(r"^v?([0-9]+\.[0-9]+\.[0-9]+)$").unwrap();
+    let semantic_version_regex = Regex::new(SEMANTIC_VERSION_TAG_PATTERN).unwrap();
 
     let tag_names = match repo.tag_names(None) {
         Ok(tag_names) => tag_names,

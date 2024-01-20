98ab881f34209710d041fc224dc8c7f9e48b6728
Seo, Sinhyeok <sinhyeok.seo@purplelabs.co.kr>
2024-01-20 13:50:11 +0900

    refactor: Optimize latest_semantic_version_tag() function

--- src/git_service.rs
+++ src/git_service.rs
@@ -6,7 +6,7 @@ pub(crate) fn open_repo() -> Result<Repository, Error> {
 }
 
 pub(crate) fn latest_semantic_version_tag(repo: &Repository) -> Option<String> {
-    let semantic_version_regex = Regex::new(r"^v?(\d+\.\d+\.\d+)$").unwrap();
+    let semantic_version_regex = Regex::new(r"^v?([0-9]+\.[0-9]+\.[0-9]+)$").unwrap();
 
     let tag_names = match repo.tag_names(None) {
         Ok(tag_names) => tag_names,
@@ -16,15 +16,10 @@ pub(crate) fn latest_semantic_version_tag(repo: &Repository) -> Option<String> {
         }
     };
 
-    let mut semantic_version_tag_names: Vec<_> = tag_names
-        .into_iter()
-        .filter(|t| semantic_version_regex.is_match(t.unwrap()))
-        .collect();
-
-    semantic_version_tag_names.sort();
-
-    match semantic_version_tag_names.into_iter().last() {
-        Some(latest_tag_name) => Some(latest_tag_name?.to_string()),
-        None => None,
-    }
+    tag_names
+        .iter()
+        .filter_map(|t| t)
+        .filter(|t| semantic_version_regex.is_match(t))
+        .last()
+        .map(|tag_name| tag_name.to_string())
 }

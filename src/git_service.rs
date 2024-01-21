cdf166a28e73d91577e297ecce857af3be2cfac3
Seo, Sinhyeok <sinhyeok.seo@purplelabs.co.kr>
2024-01-21 12:08:37 +0900

    feat: Implement git_repo.branch_name()

--- src/git_service.rs
+++ src/git_service.rs
@@ -1,10 +1,14 @@
-use git2::Repository;
+use git2::{Error, Repository};
 use regex::Regex;
 
 const SEMANTIC_VERSION_TAG_PATTERN: &str = r"^v?([0-9]+\.[0-9]+\.[0-9]+)$";
 
+fn open_repo() -> Repository {
+    Repository::open(".").unwrap_or_else(|e| panic!("Failed to open git repo: {}", e))
+}
+
 pub(crate) fn last_semantic_version_tag(default: String) -> String {
-    let repo = Repository::open(".").unwrap_or_else(|e| panic!("Failed to open git repo: {}", e));
+    let repo = open_repo();
 
     let semantic_version_regex = Regex::new(SEMANTIC_VERSION_TAG_PATTERN).unwrap();
 
@@ -22,4 +26,20 @@ pub(crate) fn last_semantic_version_tag(default: String) -> String {
         .filter(|t| semantic_version_regex.is_match(t))
         .last()
         .map_or(default, |tag_name| tag_name.to_string())
+}
+
+pub(crate) fn branch_name() -> Result<String, Error> {
+    let repo = open_repo();
+
+    let head = repo.head()?;
+
+    if head.is_branch() {
+        if let Some(branch) = head.shorthand() {
+            Ok(branch.to_string())
+        } else {
+            Err(Error::from_str("Failed to retrieve branch name"))
+        }
+    } else {
+        Err(Error::from_str("HEAD is in detached state, not pointing to branch"))
+    }
 }
--- src/pipelines/git_repo.rs
+++ src/pipelines/git_repo.rs
@@ -1,10 +1,11 @@
+use crate::git_service;
 use crate::pipelines::Pipeline;
 
 pub(crate) struct GitRepo;
 
 impl Pipeline for GitRepo {
     fn branch_name(&self) -> String {
-        "git_repo_branch_name".to_string()
+        git_service::branch_name().unwrap_or_else(|e| panic!("{}", e))
     }
 
     fn short_commit_sha(&self) -> String {
